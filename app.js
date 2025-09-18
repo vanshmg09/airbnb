c67b7c66295380c1e63ef58acf95f7ff5534ab7e
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-09-18 21:56:28 +0530

    Create Index Route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 // Require Listing model
 const Listing = require("./models/listing.js");
 
+const path = require("path");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -17,22 +19,31 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views" , path.join(__dirname,"views"));
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title : "My new Villa",
-        description : "By the Beach",
-        price : 1200,
-        location : "Calangute,Goa",
-        country : "India"
-    });
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title : "My new Villa",
+//         description : "By the Beach",
+//         price : 1200,
+//         location : "Calangute,Goa",
+//         country : "India"
+//     });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successfully testing");
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successfully testing");
+// });
+
+// Index Route
+app.get("/listings", async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("./listings/index.ejs", {allListing});
 });
 
 app.listen(8080, () => {
--- init/index.js
+++ init/index.js
@@ -5,22 +5,24 @@ const Listing = require("../models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(() => {
+        console.log("connected to DB");
+    }).catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
+    // To delete all previous Random data
+    await Listing.deleteMany({});
+    // To initialized data
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+
+}
 
 initDB();
 
