73825e21523b29cd01a0e9f948e075fbb8f65268
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-07 00:05:25 +0530

    Sending Cookies (Classroom Miscellaneous)

--- classroom/server.js
+++ classroom/server.js
@@ -3,17 +3,19 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
-
-app.use("/users", users);
-app.use("/posts", posts);
+app.get("/getcookies", (req,res) => {
+    res.cookie("greet","hello");
+    res.cookie("madeIn","India");
+    res.send("send you some cookies");
+});
 
 
 app.get("/", (req,res) => {
     res.send("Hi, I am root!");
 })
 
-
-
+app.use("/users", users);
+app.use("/posts", posts);
 
 
 app.listen(3000, () => {
