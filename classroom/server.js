a7ebb0cdf34c0bbb1829cf86a05d7fbe99914ca0
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-11 22:03:30 +0530

    Express Session Qption (Classroom Miscellaneous)

--- classroom/server.js
+++ classroom/server.js
@@ -6,11 +6,20 @@ const posts = require("./routes/post.js");
 const session = require("express-session");
 
 // EXPRESS SESSION
-app.use(session({ secret: "mysupersecretstring" }));
+app.use(session({ secret: "mysupersecretstring", resave: false, saveUninitialized: true }));
 
-app.get("/test", (req, res) => {
-    res.send("test seccessful!");
-}) 
+app.use("/reqcount", (req,res) => {
+    if(req.session.count){
+        req.session.count++;
+    }else{
+        req.session.count = 1
+    }
+    res.send(`You send request ${req.session.count} time`);
+});
+
+// app.get("/test", (req, res) => {
+//     res.send("test seccessful!");
+// }); 
 
 // To Require cookie-parser
 // const cookieParser = require("cookie-parser");
