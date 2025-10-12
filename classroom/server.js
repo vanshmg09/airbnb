f2355a4a1d332ce4826a13a06be6530abcd36b14
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-12 21:30:58 +0530

    Implement Session in Project (Classroom Miscellaneous)

--- classroom/server.js
+++ classroom/server.js
@@ -11,6 +11,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname,"views"));
 
+
 const sessionOptions = { 
     secret: "mysupersecretstring",
     resave: false, 
@@ -20,15 +21,27 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+    next();
+});
+
 app.get("/register", (req, res) => {
     let {name = "anonymous"} = req.query;
     req.session.name = name;
-    req.flash("success", "user registered successfully !");
+    if(name === "anonymous"){
+        req.flash("error", "user not registered !");
+    }else{
+        req.flash("success", "user registered successfully !");
+        
+    }
+
     res.redirect("/hello"); 
 });
 
 app.get("/hello", (req, res) => {
-    res.render("page.ejs", {name : req.session.name , msg: req.flash("success")});
+    res.render("page.ejs", {name : req.session.name});
 });
 
 // app.use("/reqcount", (req,res) => {
