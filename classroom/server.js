119c0dae0624be225ef4ca3c3943fd3b71a3a730
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-12 20:22:55 +0530

    Using connect-flash (Classroom Miscellaneous)

--- classroom/server.js
+++ classroom/server.js
@@ -4,6 +4,12 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 // To Require Express session
 const session = require("express-session");
+// To Require connect-flash
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views" , path.join(__dirname,"views"));
 
 const sessionOptions = { 
     secret: "mysupersecretstring",
@@ -12,15 +18,17 @@ const sessionOptions = {
 }
 // EXPRESS SESSION
 app.use(session(sessionOptions));
+app.use(flash());
 
 app.get("/register", (req, res) => {
     let {name = "anonymous"} = req.query;
     req.session.name = name;
+    req.flash("success", "user registered successfully !");
     res.redirect("/hello"); 
 });
 
 app.get("/hello", (req, res) => {
-    res.send(`hello, ${req.session.name}`);
+    res.render("page.ejs", {name : req.session.name , msg: req.flash("success")});
 });
 
 // app.use("/reqcount", (req,res) => {
