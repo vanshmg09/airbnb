c66ad9c21187e821b3bbce6a681d472e1fa3d8b9
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-14 19:43:50 +0530

    Flash Failure Includes

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
-    console.log(res.locals.success);
+    res.locals.error = req.flash("error");
     next();
 })
 
