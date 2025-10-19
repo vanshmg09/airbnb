3ac729d86750c26456092db46e19d535ce763d26
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-19 22:20:54 +0530

    Getting Stared wit Maps (Our First Map)

---
+++ public/js/map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([28.644800, 77.216721], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
